4210b938ab9c09fa19716620dd1871dbc81ec4de
Celena Rock <celena.r.rock@gmail.com>
2021-10-12 14:04:23 -0500

    initial commit

---
+++ calculator/calculator/calc.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
