45a1647bffcc89a34909104a62d0eb1a87d0d715
Celena Rock <celena.r.rock@gmail.com>
2021-10-12 15:34:20 -0500

    final draft

--- calculator/calculator/calc.js
+++ calculator/calculator/calc.js
@@ -7,7 +7,7 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate? ", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
@@ -15,7 +15,20 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
-	// This line closes the connection to the command line interface.
+
+
+	if (mathSymbol === '+'){
+	console.log(num1 + num2)
+} else if (mathSymbol === '-'){
+	console.log(num1 - num2)
+} else if (mathSymbol === '*'){
+	console.log(num1 * num2)
+} else if (mathSymbol === '/'){
+	console.log(num1 / num2)		
+} else if (mathSymbol === '|'){
+	console.log(Math.sqrt(num1))
+}
+// This line closes the connection to the command line interface.
 	reader.close()
 
 });
