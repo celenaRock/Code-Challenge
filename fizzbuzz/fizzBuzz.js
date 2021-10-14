2f2de57b6e4671130b9bff09059fa68ae89ec3ee
Celena Rock <celena.r.rock@gmail.com>
2021-10-14 11:51:12 -0500

    adding files for code challenges

---
+++ fizzbuzz/fizzBuzz.js
@@ -0,0 +1,15 @@
+
+for (let i=1; i < 50; i++) {
+    if(i % 3 === 0 && i % 5 ===0) {
+        console.log("fizzbuzz.")
+        }
+        else if (i % 5 ===0) {
+            console.log("buzz.")
+        } 
+        else if (i % 3 === 0) {
+            console.log("fizz.")
+        } 
+        else {
+        console.log(i);
+}
+}
